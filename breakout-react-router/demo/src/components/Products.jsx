adc1f5f7ce160b29cbf7f5abcaba0f1d40b0f9d7
Andy Lindsay <andydlindsay@gmail.com>
2024-02-06 13:37:36 -0800

    breakout over

--- breakout-react-router/demo/src/components/Products.jsx
+++ breakout-react-router/demo/src/components/Products.jsx
@@ -1,7 +1,17 @@
-const Products = () => {
+import {Link, Navigate} from 'react-router-dom';
+
+const Products = (props) => {
+  if (props.dontNavigate) {
+    return <Navigate to="/home" />;
+  }
+
   return (
     <div>
       <h2>Products Component</h2>
+      <Link to="/products/1">Product #1 </Link>
+      <Link to="/products/2">Product #2 </Link>
+      <Link to="/products/3">Product #3 </Link>
+      <Link to="/products/4">Product #4 </Link>
     </div>
   );
 };
